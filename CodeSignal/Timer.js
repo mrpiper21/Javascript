778d9d8a7050b2400e29f666b071e584c517e284
mrpiper21 <benbaah104@gmail.com>
2024-05-31 16:44:18 +0000

    update

--- CodeSignal/Timer.js
+++ CodeSignal/Timer.js
@@ -1,7 +1,25 @@
 function Timer() {
     let hr = "00";
     for (let i = 0; i < 24; i++){
-        console.log(Number(hr[1]))
+        
     }
 }
 Timer()
+
+const button = document.getElementsByTagName("button")
+const box = document.getElementsByClassName("box")
+
+button[0].onclick = () => {
+    
+}
+
+let sec = 10
+setTimeout(()=>{
+    setInterval(() => {
+        --sec
+        console.log(sec)
+        if (sec === 0) {
+            clearTimeout(sec)
+        }
+    },1000)
+},[3000])
